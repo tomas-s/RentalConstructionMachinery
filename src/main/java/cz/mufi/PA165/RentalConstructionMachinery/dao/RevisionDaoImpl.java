558b241067235d47bf358acee76ed83182b5713d
Tomas <tomas.slizik@gmail.com>
2015-10-25 21:13:21 +0100

    Revision - DAO, DAOimpl, Entity

--- src/main/java/cz/mufi/PA165/RentalConstructionMachinery/dao/RevisionDaoImpl.java
+++ src/main/java/cz/mufi/PA165/RentalConstructionMachinery/dao/RevisionDaoImpl.java
@@ -1,8 +1,30 @@
 package cz.mufi.PA165.RentalConstructionMachinery.dao;
 
+import cz.mufi.PA165.RentalConstructionMachinery.domain.Revision;
+import java.util.List;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class RevisionDaoImpl implements RevisionDao {
+
+    public List<Revision> getARevision() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void addRevision(Revision revision) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void deleteRevision(Revision revision) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Revision updateRevision(Revision revision) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Revision gedRevisionById(Revision revision) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
