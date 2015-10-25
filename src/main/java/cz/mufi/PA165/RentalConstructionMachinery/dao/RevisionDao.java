ca6da1a49ab8452e9fbabc64291644b7adcccb7b
Tomas <tomas.slizik@gmail.com>
2015-10-25 21:16:43 +0100

    Revision - DAO, DAOimpl, Entity

--- src/main/java/cz/mufi/PA165/RentalConstructionMachinery/dao/RevisionDao.java
+++ src/main/java/cz/mufi/PA165/RentalConstructionMachinery/dao/RevisionDao.java
@@ -1,5 +1,20 @@
 package cz.mufi.PA165.RentalConstructionMachinery.dao;
 
+import cz.mufi.PA165.RentalConstructionMachinery.domain.Revision;
+import java.util.List;
+/*
+*@author Tomas
+*/
 public interface RevisionDao {
+    
+    public List<Revision> getARevision();
+    
+    public void addRevision(Revision revision);
+    
+    public void deleteRevision(Revision revision);
+    
+    public Revision updateRevision(Revision revision);
+    
+    public Revision gedRevisionById(Revision revision);
 
 }
