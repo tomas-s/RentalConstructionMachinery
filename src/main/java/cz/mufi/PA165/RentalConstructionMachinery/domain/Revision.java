eac71016d9406b3286ae79fd271bb38d8a5aa10d
Tomas <tomas.slizik@gmail.com>
2015-10-25 21:17:30 +0100

    Revision - DAO, DAOimpl, Entity

--- src/main/java/cz/mufi/PA165/RentalConstructionMachinery/domain/Revision.java
+++ src/main/java/cz/mufi/PA165/RentalConstructionMachinery/domain/Revision.java
@@ -1,16 +1,37 @@
 package cz.mufi.PA165.RentalConstructionMachinery.domain;
-
+/**
+ * 
+ * @author Tomas
+ *
+ */
 import java.util.Date;
+import javax.persistence.Column;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "REVISION")
 public class Revision {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @NotNull
+    @Column(nullable = false)
     private Date revisionDate;
+    
+    
+    //tymto som si neni isty treba sa este nato pozriet
+    @ManyToOne(fetch=FetchType.LAZY)
     private Machine machine;
 
     /*
