ae8763e7d4f08e98f186c9959206cbf805ee02a1
zdenek <zdenek@localhost>
2015-10-28 14:04:06 +0100

    Customer + CustomerDaoTest

--- src/main/java/cz/mufi/PA165/RentalConstructionMachinery/domain/Customer.java
+++ src/main/java/cz/mufi/PA165/RentalConstructionMachinery/domain/Customer.java
@@ -89,6 +89,58 @@ public class Customer {
         this.customerType = customerType;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((customerType == null) ? 0 : customerType.hashCode());
+        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+        result = prime * result + ((phoneNumber == null) ? 0 : phoneNumber.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Customer other = (Customer) obj;
+        if (customerType != other.customerType)
+            return false;
+        if (firstName == null) {
+            if (other.firstName != null)
+                return false;
+        } else if (!firstName.equals(other.firstName))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (lastName == null) {
+            if (other.lastName != null)
+                return false;
+        } else if (!lastName.equals(other.lastName))
+            return false;
+        if (phoneNumber == null) {
+            if (other.phoneNumber != null)
+                return false;
+        } else if (!phoneNumber.equals(other.phoneNumber))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", phoneNumber="
+                + phoneNumber + ", customerType=" + customerType + "]";
+    }
+
     // public List<Rent> getRentHistory() {
     // return rentHistory;
     // }
--- src/test/java/cz/mufi/PA165/RentalConstructionMachinery/dao/CustomerDaoTest.java
+++ src/test/java/cz/mufi/PA165/RentalConstructionMachinery/dao/CustomerDaoTest.java
@@ -1,7 +1,11 @@
 package cz.mufi.PA165.RentalConstructionMachinery.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
 import javax.transaction.Transactional;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,26 +27,63 @@ public class CustomerDaoTest {
     @Autowired
     private CustomerDao customerDao;
 
+    @PersistenceContext
+    private EntityManager em;
+
     @Test
     @Transactional
-    public void testTest() {
-        System.out.println("dao: " + customerDao);
+    public void testCreate() {
 
-        // em.getTransaction().begin();
+        Customer created = new Customer();
+        created.setFirstName("A");
+        created.setLastName("B");
+        created.setCustomerType(CustomerType.LEGAL);
+        created.setPhoneNumber("666");
 
-        Customer customer = new Customer();
-        customer.setFirstName("A");
-        customer.setLastName("B");
-        customer.setCustomerType(CustomerType.LEGAL);
-        customer.setPhoneNumber("666");
+        customerDao.create(created);
 
-        customerDao.create(customer);
+        em.flush();
+        em.clear();
 
-        System.out.println("dao: " + customer);
-        Customer found = customerDao.findById(customer.getId());
-        System.out.println("dao: " + found);
+        Customer found = customerDao.findById(created.getId());
 
-        // em.getTransaction().commit();
+        System.out.println("created: " + created);
+        System.out.println("found: " + found);
+
+        Assert.assertEquals(created, found);
+    }
+
+    @Test(expected = Exception.class)
+    @Transactional
+    public void testNullPhoneNumber() {
+
+        Customer created = new Customer();
+        created.setFirstName("A");
+        created.setLastName("B");
+        created.setCustomerType(CustomerType.LEGAL);
+
+        customerDao.create(created);
+    }
+
+    @Test(expected = PersistenceException.class)
+    @Transactional
+    public void testUniquePhoneNumber() {
+
+        Customer created1 = new Customer();
+        created1.setFirstName("A");
+        created1.setLastName("B");
+        created1.setCustomerType(CustomerType.LEGAL);
+        created1.setPhoneNumber("666");
+
+        customerDao.create(created1);
+
+        Customer created2 = new Customer();
+        created2.setFirstName("C");
+        created2.setLastName("D");
+        created2.setCustomerType(CustomerType.LEGAL);
+        created2.setPhoneNumber("666");
+
+        customerDao.create(created2);
     }
 
 }
