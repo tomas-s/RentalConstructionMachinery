f71c1bc72fff198d2e4f771cfc67f17c374dd2d7
Zdenek Skerik <zdenek@localhost.localdomain>
2015-10-28 15:54:33 +0100

    customer test

--- src/main/java/cz/mufi/PA165/RentalConstructionMachinery/dao/CustomerDaoImpl.java
+++ src/main/java/cz/mufi/PA165/RentalConstructionMachinery/dao/CustomerDaoImpl.java
@@ -1,10 +1,5 @@
 package cz.mufi.PA165.RentalConstructionMachinery.dao;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.stereotype.Repository;
 
 import cz.mufi.PA165.RentalConstructionMachinery.domain.Customer;
@@ -16,39 +11,4 @@ import cz.mufi.PA165.RentalConstructionMachinery.domain.Customer;
  */
 @Repository
 public class CustomerDaoImpl extends DaoGenericImpl<Customer> implements CustomerDao {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    public void create(Customer customer) {
-        // TODO Auto-generated method stub
-
-        System.out.println("CREATE");
-
-        // em.getTransaction().begin();
-        em.persist(customer);
-        // em.getTransaction().commit();
-
-        System.out.println("CREATE DONE");
-
-        System.out.println(em);
-    }
-
-    public void delete(Customer customer) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void update(Customer customer) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public Customer findById(Long id) {
-        return em.find(Customer.class, id);
-    }
-
-    public List<Customer> findAll() {
-        return null;
-    }
 }
