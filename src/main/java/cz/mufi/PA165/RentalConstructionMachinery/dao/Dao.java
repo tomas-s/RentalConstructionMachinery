5f3f798eb6dc2ea983267242533a94b0f3f21225
zdenek <zdenek@localhost>
2015-10-28 14:59:48 +0100

    customer + dao comments api

--- src/main/java/cz/mufi/PA165/RentalConstructionMachinery/dao/Dao.java
+++ src/main/java/cz/mufi/PA165/RentalConstructionMachinery/dao/Dao.java
@@ -7,13 +7,39 @@ import java.util.List;
  */
 public interface Dao<T> {
 
+    /**
+     * Insert {@code T} to the DB.
+     * 
+     * @param customer
+     */
     void create(T entity);
 
+    /**
+     * Update {@code T} from DB.
+     * 
+     * @param customer
+     */
     void update(T entity);
 
+    /**
+     * Delete {@code T} from DB.
+     * 
+     * @param customer
+     */
     void delete(T entity);
 
+    /**
+     * Find {@code T} by {@code id}.
+     * 
+     * @param id
+     * @return
+     */
     T findById(long id);
 
+    /**
+     * Find all {@code T}.
+     * 
+     * @return
+     */
     List<T> findAll();
 }
--- src/main/java/cz/mufi/PA165/RentalConstructionMachinery/domain/Customer.java
+++ src/main/java/cz/mufi/PA165/RentalConstructionMachinery/domain/Customer.java
@@ -1,5 +1,6 @@
 package cz.mufi.PA165.RentalConstructionMachinery.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -46,7 +47,7 @@ public class Customer {
     private CustomerType customerType;
 
     @OneToMany(mappedBy = "customer")
-    private List<Rent> rentHistory;
+    private List<Rent> rentHistory = new ArrayList<Rent>();
 
     /*
      * Generated
@@ -100,13 +101,16 @@ public class Customer {
         this.rentHistory = rentHistory;
     }
 
+    public void addRent(Rent rent) {
+        this.rentHistory.add(rent);
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((customerType == null) ? 0 : customerType.hashCode());
         result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
         result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
         result = prime * result + ((phoneNumber == null) ? 0 : phoneNumber.hashCode());
         return result;
@@ -127,11 +131,6 @@ public class Customer {
             if (other.firstName != null)
                 return false;
         } else if (!firstName.equals(other.firstName))
-            return false;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
             return false;
         if (lastName == null) {
             if (other.lastName != null)
